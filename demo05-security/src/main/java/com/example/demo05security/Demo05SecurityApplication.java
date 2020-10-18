c9615297f184279a148c38273fc1662c907381e0
nqbefgvs <389179622@qq.com>
2020-10-18 19:09:42 +0800

    version 1.0

---
+++ demo05-security/src/main/java/com/example/demo05security/Demo05SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo05security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo05SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo05SecurityApplication.class, args);
+    }
+
+}
---
+++ demo05-security/src/test/java/com/example/demo05security/Demo05SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo05security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo05SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
